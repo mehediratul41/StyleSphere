d3bb9370f978d9b22a8842b92407f7cbf48fa9f5
Mehedi Ratul <mehediratul41@gmail.com>
2023-12-31 11:18:07 +0600

    User's role colum added to users table(migration)

---
+++ database/migrations/2023_12_31_045213_add_users_role_to_users.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //Added users role to the users table
+            $table->tinyInteger('users_role')->default(0)->after('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
