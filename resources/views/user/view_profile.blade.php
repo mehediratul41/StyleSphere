acb426855bbc6ea796d5677e21ea8d65be5af663
Mehedi Ratul <mehediratul41@gmail.com>
2024-01-02 21:28:09 +0600

    View_user page css added

--- resources/views/user/view_profile.blade.php
+++ resources/views/user/view_profile.blade.php
@@ -4,12 +4,20 @@
 @endpush
 
 @section('main_section')
+    <div class="view_profile">
         @if(session()->has('user_id'))
-        <p>User ID: {{ session('user_id') }}</p>
-        <p>User Name: {{ session('name') }}</p>
-        <p>User Email: {{ session('email') }}</p>
+        <div class="view_user_details">
+            <img src="{{asset('assets/logo.png')}}" class="user_photo" alt="User Photo">
+            <div class="user_details">
+                <p>USER ID : {{ session('user_id') }}</p>
+                <p>USER NAME : {{ session('name') }}</p>
+                <p>USER EMAIL : {{ session('email') }}</p>
+            </div>
+            <a href="{{url('/user/edit_profile')}}" class="btn btn-outline-primary edit_profile_anchor" >Edit Profile</a>
+        </div>
         <!-- Add more user details as needed -->
         @else
             <p>No user details available.</p>
         @endif
+    </div>
 @endsection
